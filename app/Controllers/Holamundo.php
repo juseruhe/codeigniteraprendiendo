ec0e7b346849f63bcacedc69da2bbce741d550fd
Sebastian <jsruiz7411@misena.edu.co>
2021-03-27 10:17:24 -0500

    iniciando codeigniter

---
+++ app/Controllers/Holamundo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+class Holamundo extends BaseController
+{
+	public function index()
+	{
+
+		$estructura = view('estructura/header').view('estructura/body');
+		
+		return $estructura;
+	}
+}
---
+++ app/Views/estructura/body.php
@@ -0,0 +1,4 @@
+<body>
+<h1>Hola desde Body</h1>
+</body>
+</html>
